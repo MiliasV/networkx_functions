a6ac7414e52a8f0fc4649ea6dbab5218de77b4f8
bill <milias.vasilis@gmail.com>
2017-04-19 15:32:57 +0200

    create edgelist per time function added

--- networkx_functions.py
+++ networkx_functions.py
@@ -35,6 +35,25 @@ def create_aggr_network(file):
                 G.add_edge(vertex1,vertex2)
     return G 
 
+def create_edgelist_per_time(file): #create edgelist 
+    timeD = {}  # Dictionary with the edgelist per time  {"0":[(1,2),(5,6)...], "1":[(4,7)...]...}
+    with open(file, "rt") as csvfile:
+        mydata = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in mydata:
+            if row[0]!="" and row[1]!="" and row[2]!="":
+
+                vertex1 = int(row[0])
+                vertex2 = int(row[1])
+                t = int(row[2])
+            
+                # Create edgelist per time - Adding one edge at a time
+                if t in timeD:
+                    timeD[t].append((vertex1, vertex2))
+                else:
+                    timeD[t]=[]
+                    timeD[t].append((vertex1, vertex2))
+    return timeD
+
 def main():
     # My code here
     pass
