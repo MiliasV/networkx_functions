610f94befee94ff1d744a6dcd728abc0a934b92b
bill <milias.vasilis@gmail.com>
2017-04-19 15:35:08 +0200

    3 new methods added

--- networkx_functions.py
+++ networkx_functions.py
@@ -54,6 +54,36 @@ def create_edgelist_per_time(file): #create edgelist
                     timeD[t].append((vertex1, vertex2))
     return timeD
 
+def plot_Dict(d, mycolor, topic, label):
+    lists = sorted(d.items()) # sorted by key, return a list of tuples
+    x, y = zip(*lists) # unpack a list of pairs into two tuples
+    x = list(range(1,len(d)+1))
+    plt.figure()
+    plt.plot(x, y,"-o", color=mycolor)
+    plt.ylabel(label)
+    plt.xlabel('Year-Month')
+    path = 'images_qa/' + topic + '/' + label + '.png'
+    plt.savefig(path)
+    plt.close("all")
+
+
+def average_dict(dictionary):
+    numbers = [dictionary[key] for key in dictionary]
+    mean_ = statistics.mean(numbers)
+    return mean_
+
+def writeToCsv(csvList, topic, file):
+    # del csvList[0]
+    csv_sorted = [csvList[0]]
+    csv_sorted.extend(sorted(csvList[1:], key = lambda tup:tup[0]))
+    csvfile = file + "/temporalMetrics_"+ topic + ".csv"
+    g = open(csvfile, 'a' ,newline='') 
+    csvwriter = csv.writer(g)
+    zip(*csv_sorted)
+    for row in csv_sorted:
+        csvwriter.writerow(row)
+    g.close()
+
 def main():
     # My code here
     pass
